263ae461ec5081871cb63665a0c420c65821702b
admin <admin@ali-9cf387c557fc.local>
2018-03-26 15:34:56 +0800

    init project sturct

---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+      	<section className="img-src">
+      	asd
+      	</section>
+      	<nav className="controller-nav">
+      	</nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
